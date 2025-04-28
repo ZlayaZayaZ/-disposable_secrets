d4c6e794824d2900617d5ee1ecbcecd84b1281f1
Vasilkova Lyubov <damirana_@mail.ru>
2025-04-28 12:11:39 +0700

    adding validation of Student instance

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from .utils import json_to_dict_list
 import os
-from typing import Optional
+from typing import Optional, List
+from .db_models import Student
 
 # Получаем путь к директории текущего скрипта
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,6 +15,11 @@ path_to_json = os.path.join(script_dir, 'students.json')
 
 app = FastAPI()
 
+class RBStudent:
+    def __init__(self, course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+        self.course: int = course
+        self.major: Optional[str] = major
+        self.enrollment_year: Optional[int] = enrollment_year
 
 
 @app.get("/")
@@ -21,48 +27,37 @@ def home_page():
     return {"message": "У тебя все получится!"}
 
 
-# @app.get("/students")
-# def get_all_students():
-#     return json_to_dict_list(path_to_json)
-
-
-# @app.get("/students/{course}")
-# def get_all_students_course(course: int):
-#     students = json_to_dict_list(path_to_json)
-#     return_list = []
-#     for student in students:
-#         if student["course"] == course:
-#             return_list.append(student)
-#     return return_list
-
-# @app.get("/students")
-# def get_all_students(course: Optional[int] = None, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
-#     students = json_to_dict_list(path_to_json)
-#     if course is None:
-#         return students
-#     else:
-#         return_list = []
-#         for student in students:
-#             if student["course"] == course:
-#                 return_list.append(student)
-#         return return_list
+@app.get("/students") 
+def get_all_students():
+    return json_to_dict_list(path_to_json)
     
 
 @app.get("/students/{course}")
-def get_all_students_course(course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+def get_all_students_course(request_body: RBStudent = Depends()) -> List[Student]:
+# def get_all_students_course(course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018) -> List[
+    # Student]:
     students = json_to_dict_list(path_to_json)
     filtered_students = []
     for student in students:
-        if student["course"] == course:
+        if student["course"] == request_body.course:
             filtered_students.append(student)
 
-    if major:
-        filtered_students = [student for student in filtered_students if student['major'].lower() == major.lower()]
+    if request_body.major:
+        filtered_students = [student for student in filtered_students if student['major'].lower() == request_body.major.lower()]
 
-    if enrollment_year:
-        filtered_students = [student for student in filtered_students if student['enrollment_year'] == enrollment_year]
+    if request_body.enrollment_year:
+        filtered_students = [student for student in filtered_students if student['enrollment_year'] == request_body.enrollment_year]
 
     return filtered_students
+
+@app.get("/student/{student_id}", response_model=Student)
+# @app.get("/student/{student_id}")
+def get_student_from_param_id(student_id: int):
+    students = json_to_dict_list(path_to_json)
+    for student in students:
+        if student["student_id"] == student_id:
+            return student
+
 
 # import uvicorn
 # from fastapi import FastAPI, HTTPException, Depends
